28943c7eb08d2e2b0c0af560ff72b412f1e740fb
motii <108280115+motii8128@users.noreply.github.com>
2023-11-24 21:28:49 +0900

    Add files via upload

--- src/component.cpp
+++ src/component.cpp
@@ -21,7 +21,7 @@ namespace jp200_demo_component{
         RCLCPP_INFO(this->get_logger(), "Open Serial port");
     }
 
-    bool JP200DemoComp::OpenPort()
+    bool JP200DemoComp::openPort(int cflag_baud)
     {
         struct termios tio;
 
@@ -30,11 +30,12 @@ namespace jp200_demo_component{
         if(fd_ < 0)
         {
             RCLCPP_ERROR(rclcpp::get_logger("jp200_demo_serial"), "Failed to open serial port!");
+            return false;
         }
 
         bzero(&tio, sizeof(tio));
 
-        tio.c_cflag =  CS8 | CLOCAL | CREAD;
+        tio.c_cflag = cflag_baud |CS8 | CLOCAL | CREAD;
         tio.c_iflag = IGNPAR;
         tio.c_oflag      = 0;
         tio.c_lflag      = 0;
@@ -48,4 +49,76 @@ namespace jp200_demo_component{
 
         return true;
     }
+
+    void JP200DemoComp::closePort()
+    {
+        if(fd_ != -1)close(fd_);
+        fd_ = -1;
+    }
+    void JP200DemoComp::clearPort()
+    {
+        tcflush(fd_, TCIFLUSH);
+    }
+    bool JP200DemoComp::setBaudRate(const int baud_rate)
+    {
+        int baud = getCFlagBaud(baud_rate);
+
+        clearPort();
+
+        if(baud <= 0)
+        {
+            openPort(B38400);
+            baud_rate_ = baud_rate;
+        }
+        else
+        {
+            baud_rate_ = baud_rate;
+            return openPort(baud);
+        }
+    }
+    int JP200DemoComp::getCFlagBaud(int baudrate)
+    {
+        switch(baudrate)
+        {
+            case 9600:
+            return B9600;
+            case 19200:
+            return B19200;
+            case 38400:
+            return B38400;
+            case 57600:
+            return B57600;
+            case 115200:
+            return B115200;
+            case 230400:
+            return B230400;
+            case 460800:
+            return B460800;
+            case 500000:
+            return B500000;
+            case 576000:
+            return B576000;
+            case 921600:
+            return B921600;
+            case 1000000:
+            return B1000000;
+            case 1152000:
+            return B1152000;
+            case 1500000:
+            return B1500000;
+            case 2000000:
+            return B2000000;
+            case 2500000:
+            return B2500000;
+            case 3000000:
+            return B3000000;
+            case 3500000:
+            return B3500000;
+            case 4000000:
+            return B4000000;
+            default:
+            return -1;
+        }
+    }
+
 }

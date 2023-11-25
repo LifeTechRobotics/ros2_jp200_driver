a224cdfe5d82ae215d30964367d0cc1d7e7b51e2
motii <108280115+motii8128@users.noreply.github.com>
2023-11-25 12:51:06 +0900

    Add files via upload

--- src/component.cpp
+++ src/component.cpp
@@ -15,10 +15,62 @@ namespace jp200_demo_component{
         RCLCPP_INFO(this->get_logger(), "Start and Get Parameter");
         declare_parameter("serial_port", "/dev/ttyACM0");
         declare_parameter("baud_rate", 115200);
+        declare_parameter("servo_num", 1);
         get_parameter("serial_port", port_name_);
         get_parameter("baud_rate", baud_rate_);
+        get_parameter("servo_num", servo_num);
 
         RCLCPP_INFO(this->get_logger(), "Open Serial port");
+        if(!openPort(baud_rate_))
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to open serial port!");
+        }
+
+
+    }
+
+    JP200Utils::JP200Cmd JP200DemoComp::get_jp200_parameter(int num)
+    {
+        JP200Utils::JP200Cmd cmd = {};
+        std::string header = num + "/";
+        declare_parameter(header +"id", 0);
+        declare_parameter(header +"control_mode", 1);
+        declare_parameter(header +"enable/target_angle", false);
+        declare_parameter(header +"enable/target_velocity", false);
+        declare_parameter(header +"enable/target_current", false);
+        declare_parameter(header +"enable/target_pwm", false);
+        declare_parameter(header +"enable/get_angle", false);
+        declare_parameter(header +"enable/get_velocity", false);
+        declare_parameter(header +"enable/get_current", false);
+        declare_parameter(header +"enable/get_pwm", false);
+        declare_parameter(header +"enable/get_mpu", false);
+        declare_parameter(header +"enable/get_amp", false);
+        declare_parameter(header +"enable/get_motor", false);
+        declare_parameter(header +"enable/get_voltage", false);
+        declare_parameter(header +"enable/get_status", false);
+        declare_parameter(header +"enable/gain_position", false);
+        declare_parameter(header +"enable/gain_velocity", false);
+        declare_parameter(header +"enable/gain_current", false);
+
+        get_parameter(header +"id", cmd.id);
+        get_parameter(header +"control_mode", cmd.control_mode);
+        get_parameter(header +"enable/target_angle", cmd.angle.enable);
+        get_parameter(header +"enable/target_velocity", cmd.velocity.enable);
+        get_parameter(header +"enable/target_current", cmd.current.enable);
+        get_parameter(header +"enable/target_pwm", cmd.pwm_enable);
+        get_parameter(header +"enable/get_angle", cmd.angle.get_state);
+        get_parameter(header +"enable/get_velocity", cmd.velocity.get_state);
+        get_parameter(header +"enable/get_current", cmd.current.get_state);
+        get_parameter(header +"enable/get_pwm", cmd.get_pwm);
+        get_parameter(header +"enable/get_mpu", cmd.get_mpu_temp);
+        get_parameter(header +"enable/get_amp", cmd.get_amp_temp);
+        get_parameter(header +"enable/get_motor", cmd.get_motor_temp);
+        get_parameter(header +"enable/get_voltage", cmd.get_voltage);
+        get_parameter(header +"enable/get_status", cmd.get_status);
+        get_parameter(header +"enable/gain_position", cmd.position_gain.enable);
+        get_parameter(header +"enable/gain_velocity", cmd.velocity_gain.enable);
+        get_parameter(header +"enable/gain_current", cmd.current_gain.enable);
+        
     }
 
     bool JP200DemoComp::openPort(int cflag_baud)
@@ -60,9 +112,11 @@ namespace jp200_demo_component{
         tcflush(fd_, TCIFLUSH);
     }
 
-    int JP200DemoComp::readPort(uint8_t *buffer, int length)
+    int JP200DemoComp::readPort(std::string *rx_packet)
     {
-        return read(fd_, buffer, length);
+        uint8_t checksum = 0;
+        uint8_t rx_length = 0;
+        uint8_t wait_length = 6;
     }
 
     int JP200DemoComp::writePort(std::string tx_packet)

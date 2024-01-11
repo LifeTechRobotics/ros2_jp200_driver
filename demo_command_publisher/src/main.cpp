592de2884ffa3a4ab350a9e37776683cda7f23cb
motii <108280115+motii8128@users.noreply.github.com>
2024-01-11 22:59:23 +0900

    Add files via upload

--- demo_command_publisher/src/main.cpp
+++ demo_command_publisher/src/main.cpp
@@ -5,7 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
-#include "jp200_msgs/msg/jp200.hpp"
+#include "jp200_msgs/msg/multi_jp200.hpp"
 
 using namespace std::chrono_literals;
 
@@ -15,32 +15,32 @@ class DemoCommandPublisher : public rclcpp::Node
     DemoCommandPublisher()
     : Node("demo_command_publisher")
     {
-      publisher_ = this->create_publisher<jp200_msgs::msg::JP200>("/cmd", 10);
+      publisher_ = this->create_publisher<jp200_msgs::msg::MultiJP200>("/cmd", 10);
       timer_ = this->create_wall_timer(1000ms, std::bind(&DemoCommandPublisher::callback, this));
     }
 
   private:
     void callback()
     {
-      auto message = jp200_msgs::msg::JP200();
-      message.id = 1;
-      message.control_mode = 1;
+      auto message = jp200_msgs::msg::MultiJP200();
+      message.servos[0].id = 1;
+      message.servos[0].control_mode = 1;
 
       // set pwm(%)
-      message.enable_pwm = true;
-      message.pwm_cmd = 20;
+      message.servos[0].enable_pwm = true;
+      message.servos[0].pwm_cmd = 20;
       
       // set angle command
-      message.angle_cmd.enable = true;
-      message.angle_cmd.value = count;
+      message.servos[0].angle_cmd.enable = true;
+      message.servos[0].angle_cmd.value = count;
 
-      message.state.enable_get_angle = true;
+      message.servos[0].state.enable_get_angle = true;
 
       publisher_->publish(message);
       count += 10;
     }
 
-    rclcpp::Publisher<jp200_msgs::msg::JP200>::SharedPtr publisher_;
+    rclcpp::Publisher<jp200_msgs::msg::MultiJP200>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     size_t count;
 };
